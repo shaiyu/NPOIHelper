2f279bbe97ff648f4e5be1f89bc840f6c0dfb3d8
Labbor <labbor@sxkid.com>
2021-08-05 11:40:07 +0800

    修改示例

---
+++ NPOIHelperTest/Model/ImportRecommend.cs
@@ -0,0 +1,19 @@
+﻿using NPOIHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOIHelperTest.Model
+{
+    public class ImportRecommend
+    {
+        [TitleName("开始时间")]
+        public DateTime StartTime { get; set; }
+
+        [TitleName("结束时间")]
+        public DateTime EndTime { get; set; }
+
+    }
+}
