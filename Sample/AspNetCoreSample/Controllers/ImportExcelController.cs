f734c3ebc9199e9386fab534926d40eeffc4faea
Labbor <labbor@sxkid.com>
2021-08-05 11:40:15 +0800

    修改示例

---
+++ Sample/AspNetCoreSample/Controllers/ImportExcelController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NPOIHelper;
+using NPOIHelperWebExtension;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreSample.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ImportExcelController : ControllerBase
+    {
+        [HttpPost]
+        public string Import()
+        {
+            var file = Request.Form.Files[0];
+            var contentType = file.ContentType;
+            using (var stream = file.OpenReadStream())
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                var reader = NPOIHelper.NPOIHelperBuild.GetReader(stream, contentType);
+                var schoolRecommends = reader.ReadSheet<ImportDto>(sheetIndex: 0).ToList();
+            }
+            return "success";
+        }
+
+
+        [HttpPost]
+        public string Import2()
+        {
+            var reader = NPOIHelper.NPOIHelperBuild.GetReader("Temp/8c5ca2c1-58ae-4679-8a18-7f8784265853.xlsx");
+            var schoolRecommends = reader.ReadSheet<ImportDto>(sheetIndex: 0).ToList();
+            return "success";
+        }
+    }
+}
---
+++ Sample/AspNetCoreSample/Models/ImportDto.cs
@@ -0,0 +1,29 @@
+﻿using NPOIHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AspNetCoreSample.Models
+{
+    public class ImportDto
+    {
+        [TitleName("推荐类型")]
+        public string Type { get; set; }
+
+        [TitleName("推荐机构/课程id")]
+        public string IdsString { get; set; }
+
+        [TitleName("投放学校id")]
+        public Guid? SId { get; set; }
+
+        [TitleName("投放学部id")]
+        public string ExtIdsString { get; set; }
+
+        [TitleName("开始时间")]
+        public DateTime StartTime { get; set; }
+
+        [TitleName("结束时间")]
+        public DateTime EndTime { get; set; }
+    }
+}
