c1374ef34f6aa494f4e700d7a8df90d99eb61e4f
Labbor <labbor@sxkid.com>
2021-08-10 17:58:35 +0800

    获取Excel导出帮助类可以设置FileName

--- NPOIHelper/NPOIHelperBuild.cs
+++ NPOIHelper/NPOIHelperBuild.cs
@@ -25,12 +25,37 @@ namespace NPOIHelper
             return new HSSFExcelHelper() { Type = type };
         }
 
+        /// <summary>
+        /// 获取Excel导出帮助类
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IExcelHelper GetHelper(string fileName, NPOIType type = NPOIType.xlsx)
+        {
+            if (type == NPOIType.xlsx)
+            {
+                return new XSSFExcelHelper() { Type = type, FileName = fileName };
+            }
+            return new HSSFExcelHelper() { Type = type, FileName = fileName };
+        }
+
+        /// <summary>
+        /// 获取Excel读取帮助类
+        /// </summary>
+        /// <param name="excelFileName"></param>
+        /// <returns></returns>
         public static ExcelReader GetReader(string excelFileName)
         {
             var type = NPOITypeUtils.GetType(excelFileName);
             return new ExcelReader(excelFileName, type);
         }
 
+        /// <summary>
+        /// 获取Excel读取帮助类
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
         public static ExcelReader GetReader(Stream stream, string contentType)
         {
             var type = NPOITypeUtils.GetTypeByContentType(contentType);
